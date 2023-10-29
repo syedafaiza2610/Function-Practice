ad184db907c3d52e92c0c230c1375b2ae6badd9f
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-29 21:18:08 +0500

    first commit

---
+++ function.js
@@ -0,0 +1,33 @@
+// 1.	Create a block of code that you can use several times.
+
+// function add(a, b) {
+//     return a + b;
+//   }
+//   var result1 = add(2,6);
+//   alert(result1);
+//   var  result2 = add(56, 20);
+//   alert(result2);
+
+  
+// 6.	 Write a function that squares its argument.
+
+// function squareNumber(number) {
+//     return number * number;
+//   }
+//   const inputNumber = 5;
+//   const squared = squareNumber(inputNumber);
+//   alert(`The square of ${inputNumber} is ${squared}`);
+
+
+// 7.	 Write a function that computes factorial of a number. //going back till 1 
+//   function factorial(number) {
+//     if (number === 0) {
+//       return 1;
+//     } else {
+//       return number * factorial(number - 1);
+//     }
+//   }
+//   const num = 5;
+//   const result = factorial(num);
+//   console.log(`The factorial of ${num} is ${result}`); // Output: "The factorial of 5 is 120"
+  
