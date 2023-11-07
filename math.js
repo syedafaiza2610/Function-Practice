49b3ae9306301fd836feb2af4c1d1c1802b91fdd
syeda Faiza <syedafaiza2610@gmail.com>
2023-11-07 22:28:30 +0500

    date method

--- math.js
+++ math.js
@@ -160,11 +160,132 @@
 // 1, 1970 and assigns it to a variable that hasn't been declared 
 // beforehand. Use any variable you like to represent the Date object.
 
-// const now = new Date();
-// const minutesSince1970 = new Date("January 1, 1970");
-// const newdate = minutesSince1970.getMinutes();
+// var d = new Date();
+// var b = d.toDateString();
+// console.log(b)
 
-// console.log(newdate);
+
+// var d = new Date();
+// var  a = d.getMilliseconds('January 1, 1970');
+// console.log(d.getMilliseconds()  + a)
+
+
+// 19. Create a date object of the starting date of this Ramadan and alert 
+// the number of days past since 1st Ramadan?
+// Note: 1st Ramadan was on June 18, 2015
+
+// // Define the starting date of 1st Ramadan
+// const ramadanStartDate = new Date('2015-06-18');
+
+// // Get the current date
+// const currentDate = new Date();
+
+// // Calculate the time difference in milliseconds
+// const timeDifference = currentDate - ramadanStartDate;
+
+// // Convert the time difference to days
+// const daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+// // Alert the number of days passed since 1st Ramadan
+// alert(`Number of days passed since 1st Ramadan: ${daysPassed} days`);
+
+// 18. Write a program that creates a Date object for the last day of the 
+// last month of 2020 and assigns it to variable named laterDate
+
+
+
+// var later = new Date("December 31, 2023");
+// console.log(later);
+
+
+// 20. Write a program that displays in your browser the seconds that 
+// elapsed between the reference date and the beginning of 2015.
+
+
+
+// // Define the reference date (December 5, 2015)
+// const referenceDate = new Date('2015-12-05');
+
+// // Define the beginning of 2015
+// const beginningOf2015 = new Date('2015-01-01');
+
+// // Calculate the time difference in milliseconds
+// const timeDifference = referenceDate - beginningOf2015;
+
+// // Convert the time difference to seconds
+// const secondsElapsed = Math.floor(timeDifference / 1000);
+
+// // Display the number of seconds elapsed in the browser
+// document.body.innerHTML = `Seconds elapsed between December 5, 2015, and the beginning of 2015: ${secondsElapsed} seconds`;
+
+// 17. Write a program that tests whether it's before noon and alert “Its 
+// AM” else “its PM”
+
+// // Get the current date and time
+// const currentDate = new Date();
+
+// // Get the current hour (in 24-hour format)
+// const currentHour = currentDate.getHours();
+
+// // Check if it's before noon (AM) or not (PM)
+// if (currentHour < 12) {
+//   alert("It's AM");
+// } else {
+//   alert("It's PM");
+// // }
+
+
+// 23. Write a program to ask the user about his age. Calculate and 
+// show his birth year in your browser
+
+// // Ask the user for their age
+// const age = prompt("Please enter your age:");
+
+// // Calculate the birth year
+// const currentYear = new Date().getFullYear();
+// const birthYear = currentYear - parseInt(age);
+
+// // Display the birth year in the browser
+// alert(`Your birth year is approximately ${birthYear}`);
+
+
+
+
+
+// // Get input from the user
+// const customerName = prompt("Enter Customer Name:");
+// const currentMonth = prompt("Enter Current Month:");
+// const numberOfUnits = parseFloat(prompt("Enter Number of Units:"));
+// const chargesPerUnit = parseFloat(prompt("Enter Charges per Unit:"));
+// const dueDate = prompt("Enter Due Date (yyyy-mm-dd):");
+// const latePaymentSurchargeRate = parseFloat(prompt("Enter Late Payment Surcharge Rate (%):"));
+
+// // Calculate net amount payable
+// const netAmountPayable = (numberOfUnits * chargesPerUnit).toFixed(2);
+
+// // Calculate late payment surcharge
+// const currentDate = new Date();
+// const dueDateObj = new Date(dueDate);
+// const latePaymentSurcharge = (currentDate > dueDateObj) ? ((netAmountPayable * latePaymentSurchargeRate) / 100).toFixed(2) : "0.00";
+
+// // Calculate gross amount payable
+// const grossAmountPayable = (parseFloat(netAmountPayable) + parseFloat(latePaymentSurcharge)).toFixed(2);
+
+// // Display the bill in the browser
+// document.write("<h1>K-Electric Bill</h1>");
+// document.write("<p>Customer Name: " + customerName + "</p>");
+// document.write("<p>Current Month: " + currentMonth + "</p>");
+// document.write("<p>Number of Units: " + numberOfUnits.toFixed(2) + "</p>");
+// document.write("<p>Charges per Unit: $" + chargesPerUnit.toFixed(2) + "</p>");
+// document.write("<p>Net Amount Payable (within Due Date): $" + netAmountPayable + "</p>");
+// document.write("<p>Late Payment Surcharge: $" + latePaymentSurcharge + "</p>");
+// document.write("<p>Gross Amount Payable (after Due Date): $" + grossAmountPayable + "</p>");
+
+
+
+
+
+
 
 
 
